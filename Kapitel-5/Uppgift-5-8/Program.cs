48fdd017cc848afe564587c839862a6c138bf862
M8H8lol <70271361+M8H8lol@users.noreply.github.com>
2020-12-03 10:46:49 +0100

    Repetition av arrayer

---
+++ Kapitel-5/Uppgift-5-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_5_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening");
+
+            string mening = Console.ReadLine();
+
+            string[] ord = mening.Split(' ');
+
+            foreach (var ordet in ord)
+            {
+                Console.WriteLine(ordet);
+            }
+        }
+    }
+}
