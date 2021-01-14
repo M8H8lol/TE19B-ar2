07139f5c168fb19332e4cd9272967f2fbbec4eee
M8H8lol <70271361+M8H8lol@users.noreply.github.com>
2021-01-14 11:09:50 +0100

    Filer

---
+++ Labbar/FIlhantering2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FIlhantering2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Filnamnet
+            string filnamn = "./animals.txt";
+
+            // Läs in alla rader
+            string[] rader = File.ReadAllLines(filnamn);
+
+            // Loopa igenom arrayen
+            // GÅ igenom rad för rad
+            int counter = 1;
+            foreach (var rad in rader)
+            {
+                Console.WriteLine(counter + "" + rad);
+                counter++;
+            }
+
+            Console.WriteLine($"hittade {counter + 1} djur");
+        }
+    }
+}
---
+++ Labbar/Filhanterare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Filhanterare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriva till en textfil
+            File.WriteAllText("fil1.txt","Nu snöar det igen! Kul!");
+
+            string allText = File.ReadAllText("./Filhanterare.csproj");
+
+            // Skriv ut innehållet
+            Console.WriteLine($"Innehållet: \n{allText}");
+
+            // Fråga användaren om filnamn
+            Console.Write("Ange filnamn: ");
+            string filnamn = Console.ReadLine();
+
+            // Kontrollera att filen finns!
+            if (File.Exists(filnamn))
+            {
+                // LÄs in allt
+                string texten = File.ReadAllText(filnamn);
+                Console.Write(texten);
+            }
+            else
+            {
+                Console.WriteLine($"Hittade inte filen {filnamn}");
+            }
+
+        }
+    }
+}
